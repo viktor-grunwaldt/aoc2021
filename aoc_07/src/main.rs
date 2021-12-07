310611d26a7e0ed26268a38a3f5d539dae4ab4e4
Viktor Grünwaldt <vik777gru@gmail.com>
2021-12-07 13:35:31 +0100

    aoc_07

---
+++ aoc_07/src/main.rs
@@ -0,0 +1,50 @@
+use std::cmp;
+
+fn read_file(name: &str) -> Vec<i32> {
+    std::fs::read_to_string(name)
+        .expect("file not found!")
+        .split(",")
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect()
+}
+
+fn part_one(name: &str) -> i32 {
+    let points = read_file(name);
+
+    //let's see how fast rust is
+    let min = points.iter().min().unwrap();
+    let max = points.iter().max().unwrap();
+
+    let sol = (*min..*max)
+        .map(|w| points.iter().fold(0, |acc, x| acc + (x - w).abs()))
+        .min()
+        .unwrap();
+    sol
+}
+
+fn part_two(name: &str) -> i32 {
+    let points = read_file(name);
+    fn f(x: i32) -> i32 {
+        if 0 > x {
+            panic!("invalid path");
+        }
+        x * (x + 1) / 2
+    }
+
+    let min = points.iter().min().unwrap();
+    let max = points.iter().max().unwrap();
+    //let's see how fast rust is
+    // so basically calculate all paths from min to max and pick the smallest one
+    let sol = (*min..*max)
+        .map(|w| points.iter().fold(0, |acc, x| acc + f((x - w).abs())))
+        .min()
+        .unwrap();
+    sol
+}
+
+fn main() {
+    assert_eq!(37, part_one("example.txt"));
+    println!("{}", part_one("input.txt"));
+    assert_eq!(168, part_two("example.txt"));
+    println!("{}", part_two("input.txt"));
+}
