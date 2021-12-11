41f32dc40eda42ddb6db5b4a16ceb82ff032319b
Viktor Grünwaldt <vik777gru@gmail.com>
2021-12-11 22:22:25 +0100

    haskell'ed code'

--- aoc_11/src/main.rs
+++ aoc_11/src/main.rs
@@ -8,129 +8,101 @@ fn read_file(name: &str) -> Vec<Vec<u8>> {
         .collect()
 }
 
-const MAX:usize = 10;
+const MAX: usize = 10;
 
-fn check_neigh(x:usize, y:usize) -> Vec<(usize,usize)> {
-    let mut n: Vec<(usize,usize)> = vec![];
-    for i in (x-1)..(x + 2) {
-        for j in (y-1)..(y+2) {
-            if i != x || j != y {
-                n.push((i,j));
-            }
-        }
-    }
-    n
+fn near_index(x: usize, y: usize) -> Vec<(usize, usize)> {
+    // i + i/4 generates:
+    // 1 2 3 4 6 7 8 9
+    (0..8).map(|i| (x - 1 + (i + i / 4) / 3, y - 1 + (i + i / 4) % 3)).collect()
+    
 }
 
-fn part_one(name: &str, iter:u32) -> u64 {
+fn iter_expl(tab: &mut Vec<Vec<u8>>) -> HashSet<(usize, usize)> {
+    let mut exploded: HashSet<(usize, usize)> = HashSet::new();
+    loop {
+        let mut expl: u32 = 0;
+
+        for i in 1..11 {
+            for j in 1..11 {
+                if tab[i][j] >= 10 && !exploded.contains(&(i, j)) {
+                    //expl found
+                    expl += 1;
+                    // prevent multiple expl at the same place
+                    exploded.insert((i, j)); 
+                    // increment nearby elems
+                    near_index(i, j).into_iter().for_each(|(x,y)| if tab[x][y] < 10 {tab[x][y] += 1});
+                }
+            }
+        }
+
+        if expl == 0 {
+            return exploded;
+        }
+    }
+}
+
+fn part_one(name: &str, iter: u32) -> u64 {
     let mut tab = read_file(name);
 
     //padding cuz fuck border checking
-    tab.insert(0, vec![99;MAX]);
-    tab.push(vec![99;MAX]);
-    for line in tab.iter_mut() {
-        line.insert(0, 99);
-        line.push(99);
-    }
+    tab.insert(0, vec![99; MAX]);   // fst
+    tab.push(vec![99; MAX]);        // last
+    tab.iter_mut().for_each(|l| {   // middle
+        l.insert(0, 99);
+        l.push(99)
+    });
 
     let mut sum = 0u64;
     for _ in 0..iter {
         // inc ene lvl
-        for i in 1..11 {
-            for j in 1..11 {
-                tab[i][j]+=1;
-            }
-        }
-        // check until no expl found
-        let mut flag = true;
-        let mut exploded:HashSet<(usize, usize)> = HashSet::new();
-        while flag {
-            let mut expl:u32 = 0;
+        (0..(MAX*MAX)).for_each(|i| tab[i / MAX + 1][i % MAX + 1] += 1);
 
-            for i in 1..11 {
-                for j in 1..11 {
-                    if tab[i][j] >= 10 && !exploded.contains(&(i,j)) { //expl found
-                        expl+=1;
-                        exploded.insert((i,j)); // expl happens only 1 time
-                        for (x, y) in check_neigh(i, j) {
-                            if tab[x][y] < 10 {
-                                tab[x][y] += 1;
-                            }
-                        }
-                    }
-                }
-            }
-            if expl == 0 {
-                flag = false;
-            }
-        }
+        // check until no expl found
+        let exploded = iter_expl(&mut tab);
         sum += exploded.len() as u64;
-        for (i, j) in exploded {
-            tab[i][j] = 0;
-        }
+
+        // zero out flashes
+        exploded.into_iter().for_each(|(i,j)| tab[i][j] = 0);
+
     }
-    
+
     sum
 }
 
-fn part_two(name: &str) -> u64 {
+fn part_two(name: &str) -> Option<u64> {
     let mut tab = read_file(name);
 
     //padding cuz fuck border checking
-    tab.insert(0, vec![99;MAX]);
-    tab.push(vec![99;MAX]);
-    for line in tab.iter_mut() {
-        line.insert(0, 99);
-        line.push(99);
-    }
-    // neat infinite counting loop
-    for i in 1.. { 
-        // inc ene lvl
-        for i in 1..11 {
-            for j in 1..11 {
-                tab[i][j]+=1;
-            }
-        }
-        let mut flag = true;
-        let mut exploded:HashSet<(usize, usize)> = HashSet::new();
-        // check until no expl found
-        while flag {
-            let mut expl:u32 = 0;
+    tab.insert(0, vec![99; MAX]);   // fst
+    tab.push(vec![99; MAX]);        // last
+    tab.iter_mut().for_each(|l| {   // middle
+        l.insert(0, 99);
+        l.push(99)
+    });
 
-            for i in 1..11 {
-                for j in 1..11 {
-                    if tab[i][j] >= 10 && !exploded.contains(&(i,j)) { //expl found
-                        expl+=1;
-                        exploded.insert((i,j)); // expl happens only 1 time
-                        for (x, y) in check_neigh(i, j) {
-                            if tab[x][y] < 10 {
-                                tab[x][y] += 1;
-                            }
-                        }
-                    }
-                }
-            }
-            if expl == 0 { // stop when no more expl
-                flag = false;
-            }
+    // neat infinite counting loop
+    for day in 1.. {
+        // inc energy lvl
+        (0..100).for_each(|i| tab[i / MAX + 1][i % MAX + 1] += 1);
+
+        // check until no expl found
+        let exploded = iter_expl(&mut tab);
+
+        if exploded.len() == 100 {
+            // end if all expl
+            return Some(day);
         }
-        if exploded.len() == 100 { // end if all expl
-            return i;
-        }
-        for (i, j) in exploded {
-            tab[i][j] = 0;
-        }
+        // zero out flashes
+        exploded.into_iter().for_each(|(i,j)| tab[i][j] = 0);
     }
-    
-    0 // should never happen but rust is picky
+
+    None // should never happen but rust is picky
 }
 
 fn main() {
+    assert_eq!(1656, part_one("example.txt", 100));
+    println!("{}", part_one("input.txt", 100));
 
-    assert_eq!(1656,part_one("example.txt", 100));
-    println!("{}",part_one("input.txt", 100));
-    
-    
-    assert_eq!(195,part_two("example.txt"));
-    println!("{}",part_two("input.txt"));
+    assert_eq!(Some(195), part_two("example.txt"));
+    println!("{}", part_two("input.txt").unwrap());
 }
